a1cbf9e00f4bc6578091d14f41e512a50ac2884a
iwenli <iwenli@IWENLI-UB>
2021-05-06 12:28:15 +0800

    EF Core Console

---
+++ EFCore/Core/BloggingContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Core
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=192.168.1.29;Initial Catalog=Blogging;User Id=sa;Password=Pa@@wordSAGerberDev;Max Pool Size=512;Min Pool Size=5;");
+            //base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+    public class Blog {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
+        public Blog Blog { get; set; }
+    }
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,42 @@
+﻿using EFCore.Core;
+using System;
+using System.Linq;
+using System.Text.Json;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+
+            Console.WriteLine("Hello World!");
+            using var db = new BloggingContext();
+            var blog = new Blog { Url = "https://baidu.com/id=" + Guid.NewGuid().ToString() };
+            db.Add(blog);
+            db.SaveChanges();
+            Console.WriteLine("写入:" + JsonSerializer.Serialize(blog, options));
+
+            var blog1 = db.Blogs.OrderBy(m => m.Id).FirstOrDefault();
+            Console.WriteLine("读取:" + JsonSerializer.Serialize(blog1, options));
+
+            blog1.Url = "https://baidu.com/id=update";
+            blog1.Posts.Add(new Post
+            {
+                Title = "Hello world!",
+                Content = "I wrote an app using EF Core!"
+            });
+            db.SaveChanges();
+            Console.WriteLine("更新:" + JsonSerializer.Serialize(blog1, options));
+
+            db.Remove(blog);
+            db.SaveChanges();
+        }
+    }
+}
