1f80fd553c3e7269c5a15b0af99f53fcb9736d38
iwenli <iwenli@IWENLI-UB>
2021-05-10 15:13:17 +0800

    signalR TS

---
+++ EFCore.SignalR.TS/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace EFCore.SignalR.TS.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(long username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
---
+++ EFCore.SignalR.TS/Startup.cs
@@ -0,0 +1,50 @@
+using EFCore.SignalR.TS.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.SignalR.TS
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/hub");
+            });
+        }
+    }
+}
