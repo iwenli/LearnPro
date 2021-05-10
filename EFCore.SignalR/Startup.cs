be4a5f44f7feac78670ffe3e436c54e4e0a08d10
iwenli <iwenli@IWENLI-UB>
2021-05-10 14:51:29 +0800

    ok

--- EFCore.SignalR/Startup.cs
+++ EFCore.SignalR/Startup.cs
@@ -1,3 +1,4 @@
+using EFCore.SignalR.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace EFCore.SignalR
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +52,7 @@ namespace EFCore.SignalR
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
