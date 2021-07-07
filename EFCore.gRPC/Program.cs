f684a9c44c873d16034ffd3b5ed419132c3856dd
iwenli <iwenli@IWENLI-UB>
2021-07-07 12:21:28 +0800

    last

--- EFCore.gRPC/Program.cs
+++ EFCore.gRPC/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,10 @@ namespace EFCore.gRPC
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context,config)=> { 
+                    //config.AddXmlFile()
+                    //config.AddEnvironmentVariables()
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- EFCore.gRPC/Startup.cs
+++ EFCore.gRPC/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,27 @@ using System.Threading.Tasks;
 
 namespace EFCore.gRPC
 {
+    public class PositionOptions
+    {
+        public const string Position = "Position";
+
+        public string Title { get; set; }
+        public string Name { get; set; }
+    }
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<PositionOptions>(Configuration.GetSection(
+                                                PositionOptions.Position));
             services.AddGrpc();
         }
 
